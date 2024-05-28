ba6a522dbd2fbf10a2b27ef946fb37d339081b0f
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-28 14:57:19 -0500

    Add files via upload

---
+++ Bottles of Root Beer/js.js
@@ -0,0 +1,11 @@
+var button = document.createElement("button");
+button.innerHTML = "run";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+var number1 = document.gerElementByld("number").value;
+
+var.text = document.createTextNode(number1 + " bottles of root beer on the wall " + number1 + " bottles of root beer take one down pass it around " + (number1 - 1) + " bottles of root beer on the wall.")
+
+//button.addEventListener("click", function() { alert(x + " bottles of root beer on the wall " + x + " bottles of root beer take one down pass it around " + x - 1 + " bottles of root beer on the wall.")});
